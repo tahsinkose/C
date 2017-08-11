6357ded6e5b70aba38d3ef7544807c353ec6385a
tahsincan köse <tahsinkose@yandex.com>
2017-08-11 23:56:31 +0300

    Output interface added.

--- cardshuffle and deal-match.c
+++ cardshuffle and deal-match.c
@@ -3,24 +3,80 @@
 #include <time.h>
 #include "card.h"
 
+
+
+
+void printHandStatus(int result){
+	switch(result){
+		case STRAIGHT:
+			printf("Hand has a STRAIGHT\n");
+			break;
+		case FLUSH:
+			printf("Hand has a FLUSH\n");
+			break;
+		case FOURTET:
+			printf("Hand has a FOURTET\n");
+			break;
+		case FULL_HOUSE:
+			printf("Hand has a FULL_HOUSE\n");
+			break;
+		case TRIPLET:
+			printf("Hand has a TRIPLET\n");
+			break;
+		case 2:
+			printf("Hand has a DOUBLE PAIR\n");
+			break;
+		case 1:
+			printf("Hand has a PAIR\n");
+			break;
+		default:
+			;
+	}
+}
+
+
+void printFinalStatus(int final){
+	switch(final){
+		case P1_WINS:
+			printf("Player 1 wins by superior hand.");
+			break;
+		case P2_WINS:
+			printf("Player 2 wins by superior hand.");
+			break;
+		case P1_WITHC:
+			printf("Player 1 wins by strong hand.");
+			break;
+		case P2_WITHC:
+			printf("Player 2 wins by strong hand.");
+			break;
+		case TIE:
+			printf("Tie.");
+			break;
+		default:
+			;
+	}
+	puts("");
+}
+
 int main()
 {
 	
 	int x,y;
 	int check1=13,check2=13;//13 corresponds to column number which is between 0 and 9. Meaningly, I have made it to explicitly unreachable at first.
-	unsigned int bigDeck[CARDS+1]={0};
 	
-	unsigned int deck[SUITS][FACES]={0};
+	
+	
 	srand(time (NULL));
 
-	shuffle(deck);
+	shuffle();
 	printf("-----------------%s-------------------------\n","PLAYER ONE'S HAND");
-	x=deal(deck,&check1,bigDeck);
-	printf("check1 = %d\n",check1);
+	x=deal(&check1);
+	printHandStatus(x);
 	
 	printf("-----------------%s-------------------------\n","PLAYER TWO'S HAND");
-	y=deal(deck,&check2,bigDeck);	
-	printf("check2=%d\n",check2);
+	y=deal(&check2);
+	printHandStatus(y);
+		
 	printf("--------------------------------------------\n");
 	if(x==0 && y==0)
 	{
@@ -29,8 +85,8 @@ int main()
 	}
 	else
 	{
-		printf("x= %d , y=%d\n",x,y);
-		compare(x,y,&check1,&check2);
+		int final = compare(x,y,check1,check2);
+		printFinalStatus(final);
 	}
 
-}//end main
+}
