7e8cad929b62b2773577a97623d528c404f937b7
Tahsincan Köse <tahsinkose@yandex.com>
2018-04-08 01:23:47 +0300

    Static array is replaced with Dynamic Array
    Redundant codes are removed.

--- 1000bruteknight.c
+++ 1000bruteknight.c
@@ -8,14 +8,19 @@ int main()
 {
 	
 	int arr[1000]={0};
-	int horizontal[SIZE],vertical[SIZE];
-	horizontal[0]=2,horizontal[1]=1,horizontal[2]=-1,horizontal[3]=-2,horizontal[4]=-2,horizontal[5]=-1,horizontal[6]=1,horizontal[7]=2;
-	vertical[0]=-1,vertical[1]=-2,vertical[2]=-2,vertical[3]=-1,vertical[4]=1,vertical[5]=2,vertical[6]=2,vertical[7]=1;
+	int horizontal[SIZE] = {2,1,-1,-2,-2,-1,1,2};
+    	int vertical[SIZE] = {-1,-2,-2,-1,1,2,2,1};
 	srand(time( NULL ));
 	for(int k=0;k<1000;k++) 
 	{
 		int move=0;
-		int chessboard[SIZE][SIZE]={0};// all squares are empty.
+        int ** chessboard;
+        chessboard = (int **)malloc(sizeof(int*)*SIZE);
+        for(int i=0;i<SIZE;i++)
+            chessboard[i] = (int *)malloc(sizeof(int)*SIZE);
+        for(int i=0;i<SIZE;i++)
+            for(int j=0;j<SIZE;j++)
+                chessboard[i][j] = 0;
 		int currentRow=3,currentColumn=3;//starting point
 		
 		chessboard[currentRow][currentColumn]=1;
@@ -26,8 +31,9 @@ int main()
 		{
 			for(int k=0;k<SIZE;k++)
 			{
-				if((chessboard[currentRow+vertical[k]][currentColumn+horizontal[k]]==0) && currentRow+vertical[k]<=7 && currentRow+vertical[k]>=0 && currentColumn+horizontal[k]<=7 && currentColumn+horizontal[k]>=0)
-				{
+				if(currentRow+vertical[k]<=7 && currentRow+vertical[k]>=0 && 
+                    currentColumn+horizontal[k]<=7 && currentColumn+horizontal[k]>=0 &&
+                    (chessboard[currentRow+vertical[k]][currentColumn+horizontal[k]]==0)){
 					loopStatus=0;
 					break;
 				}
@@ -36,39 +42,30 @@ int main()
 			if(loopStatus==0)
 			{	
 				move = rand() % 8;
-				if(currentRow+vertical[move]<=7 && currentRow+vertical[move]>=0 && currentColumn+horizontal[move]<=7 && currentColumn+horizontal[move]>=0)
-				{
-			
+				if(currentRow+vertical[move]<=7 && currentRow+vertical[move]>=0 && 
+                currentColumn+horizontal[move]<=7 && currentColumn+horizontal[move]>=0){
 					if(chessboard[currentRow+vertical[move]][currentColumn+horizontal[move]]!=1)
 					{
 						currentRow+=vertical[move];
 						currentColumn+=horizontal[move];
 						chessboard[currentRow][currentColumn]=1;
 						counter++;
-						
 					}//end if
-					
 				}//end if
 			}//end if
 			else
-			{
 				break;
-			}//end else
-			
 		}//end while
 		arr[counter]++;
+
+        for(int i=0;i<SIZE;i++)
+            free(chessboard[i]);
+        free(chessboard);
 	}//end for
 	printf("%s-------%10s\n","Length","Times");
 	for(int a=0;a<1000;a++)
 	{
-		if(arr[a]==0)
-		{
-		}
-		else
-		{
+		if(arr[a])
 			printf("%d-------%10d\n",a,arr[a]);
-		}//end else
 	}//end for
 }//end main
-
-
